74898cc3844beb500731d1ea8863ddec32ac8499
mathewgut <mathewgutoskie@gmail.com>
2024-07-11 14:04:52 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const container = document.createElement('div');
+const display = document.createElement('div');
+const displayText = document.createElement('p');
+const buttonContainer = document.createElement('div');
+
+display.setAttribute('class','display');
+display.setAttribute('id','screen');
+container.setAttribute('class','container');
+displayText.setAttribute('id','display-text');
+buttonContainer.setAttribute('class','button-container')
+
+displayText.textContent = 'numbers here';
+
+
+
+const totalHeight = 500;
+const totalWidth = 450;
+console.log(totalHeight/5+'px');
+for(let rows = 0; rows < 5; rows++){
+    const row = document.createElement('div');
+    row.setAttribute('id',`row-${rows}`)
+    row.setAttribute('class','row');
+    for(let inputs = 0; inputs < 4; inputs++){
+        const input = document.createElement('div');
+        input.setAttribute('id',`input-${inputs}-${rows}`)
+        input.setAttribute('class','input-buttons');
+        input.style.height = (totalHeight/5+'px');
+        input.style.width = (totalWidth/4+'px');
+        console.log(totalHeight/5);
+        row.appendChild(input);
+
+    }
+    buttonContainer.appendChild(row);
+}
+
+
+display.appendChild(displayText);
+container.appendChild(display);
+container.appendChild(buttonContainer);
+document.body.appendChild(container);
+
+
+
+
+
