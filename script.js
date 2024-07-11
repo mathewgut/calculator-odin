dd1ee3ccc0190cb4db69201292370a1147da8b4a
mathewgut <mathewgutoskie@gmail.com>
2024-07-11 17:25:12 -0400

    added operator buttons

--- script.js
+++ script.js
@@ -11,32 +11,36 @@ buttonContainer.setAttribute('class','button-container')
 
 displayText.textContent = 'numbers here';
 
+document.addEventListener("DOMContentLoaded", () => {
+    createButtonGrid();
+  });
 
+function createButtonGrid(){
+    const totalHeight = 500;
+    const totalWidth = 450;
+    console.log(totalHeight/5+'px');
+    for(let rows = 0; rows < 5; rows++){
+        const row = document.createElement('div');
+        row.setAttribute('id',`row-${rows}`)
+        row.setAttribute('class','row');
+        for(let inputs = 0; inputs < 4; inputs++){
+            const input = document.createElement('div');
+            input.setAttribute('id',`input-${inputs}-${rows}`)
+            input.setAttribute('class','input-buttons');
+            input.style.height = (totalHeight/5+'px');
+            input.style.width = (totalWidth/4+'px');
+            console.log(totalHeight/5);
+            row.appendChild(input);
 
-const totalHeight = 500;
-const totalWidth = 450;
-console.log(totalHeight/5+'px');
-for(let rows = 0; rows < 5; rows++){
-    const row = document.createElement('div');
-    row.setAttribute('id',`row-${rows}`)
-    row.setAttribute('class','row');
-    for(let inputs = 0; inputs < 4; inputs++){
-        const input = document.createElement('div');
-        input.setAttribute('id',`input-${inputs}-${rows}`)
-        input.setAttribute('class','input-buttons');
-        input.style.height = (totalHeight/5+'px');
-        input.style.width = (totalWidth/4+'px');
-        console.log(totalHeight/5);
-        row.appendChild(input);
-
+        }
+        buttonContainer.appendChild(row);
     }
-    buttonContainer.appendChild(row);
+    container.appendChild(buttonContainer);
 }
-
 
 display.appendChild(displayText);
 container.appendChild(display);
-container.appendChild(buttonContainer);
+
 document.body.appendChild(container);
 
 
